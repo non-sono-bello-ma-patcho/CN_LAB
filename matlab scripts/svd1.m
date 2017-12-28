6a11a36d21a00fd45c7bb3053fa4c28218cb7f52
rollingflamingo <andrea.storace.uni@gmail.com>
2017-12-28 18:10:01 +0100

    Added second part of svd drill

--- matlab scripts/svd1.m
+++ matlab scripts/svd1.m
@@ -7,8 +7,13 @@ A = init(m);
 At = A';
 
 %Computing singular values decompsition;
-svdA = svd(A);
-svdAt = svd(A');
+%sxValues svd dxValues:
+[U,svdA,V]=svd(A);
+[Ut, svdAt, Vt]=svd(A');
+ImA = orth(A);
+ImAt = orth(A');
+KerA = null(A);
+KerAt = null(A');
 % Computing egeinvalue:
 eigAAt = eig(A*At); % A*At has dim: 14x3 3x14 -> 14x14
 eigAtA = eig(At*A); % -> 3x3
@@ -17,11 +22,19 @@ eigAtA = eig(At*A); % -> 3x3
 % characteristic polynomial(?). Also, this means AAt and AtA are
 % diagonalisable => AAt = AAt' and AtA = AtA'
 
+
+% comparison between ImA and Left Singular Values (U): same value for the
+% first 3 columns;
+% comparison between ImAt and Left Singluar Values (Ut): exact same value
+
+% comparison between KerA and V: KerA nucleo banale, V 3x3 matrix
+% comparison between KerAt and Vt: 
+
 function M = init (n)
-    M = zeros(n, 3);
+    M = ones(n, 3);
     for i=1:n
         for j=1:3
-           M(i,j)=(1/i)^(j-1); 
+           M(i,j)=M(i,j)/(i^(j-1)); 
         end
     end
 end
