0e52fd0d42fedea35e368f8e7c0f80ef270947e8
rollingflamingo <andrea.storace.uni@gmail.com>
2018-02-19 16:29:55 +0100

    Improved eigenvalue script

--- matlab scripts/eigenvalue.m
+++ matlab scripts/eigenvalue.m
@@ -7,20 +7,30 @@ n = 10 * (d1+1) + d0;
 % Create Jordan block:
 A=diag(ones(1, n-1), 1) + eye(n);
 B = createPertubate(A, n);
+[Aeig, Beig, Ceig, norm1, norm2] = compute_eig_n_norm(A, B, n);
+At = A';
+Bt = B';
+[Ateig, Bteig, Cteig, normt1, normt2] = compute_eig_n_norm(At, Bt, n);
+function [Aeig, Beig, Ceig, norm1, norm2] = compute_eig_n_norm(A, B, n)
 % Computing A eigenvalues:
 Aeig = eig(A);
 
 % Computing B eigenvalues:
 Beig = eig(B);
 
+% eigenvalue comparison:
+Ceig = zeros(size(Beig));
+for i=1:n
+    Ceig(i) = (Beig(i)-Aeig(i));
+end
 % Computing norm:
-norm1 = norm(A-B)/norm(A);
+norm1 = norm(B-A)/norm(A);
 norm2 = norm(Beig-Aeig)/norm(Aeig);
-
+end
 function B = createPertubate(A, n)
     % Create pertubation:
     E = zeros(size(A));
-    E(1, n) = 10^-n;
+    E(n, 1) = 10^-n;
     %Creating pertubated Matrix:
     B = A+E;
 end
